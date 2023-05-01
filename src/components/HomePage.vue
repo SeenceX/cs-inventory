09330d5135df7e37122b1084996bdf11172903ad
Andrey <andreyvoropaev1314@gmail.com>
2023-05-01 14:40:54 +0300

    home page improvements

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,31 @@
 <template>
-    <h1>HomePage</h1>
+    <div class="containter">
+        <div class="title">
+            <div class="site-description">
+                <h2>Будь в курсе цен<br>с CS<i>Inventory</i></h2>
+                <p>Отслеживайте изменение цен на предметы в Counter-Strike<br>и управляйте своим инвентарем максимально эффективно.</p>
+            </div>
+            <form action="" class="url-form">
+                <input type="text" name="steam-url" class="url-input" id="" placeholder="Steam URL">
+                <input type="submit" class="load-input" value="Загрузить инвентарь">
+            </form>
+        </div>
+        <div class="info">
+            <div class="steps">
+                <div class="step">Загружай свой инвентарь<br>и узанай его рыночную стоимость</div>
+                <div class="step">Следи за динамикой изменения<br> стоимости своих предметов</div>
+                <div class="step">Получай максимальную<br> выгоду от продажи</div>
+            </div>
+            <div class="numbers">
+                <div class="number">1</div>
+                <div class="line"></div>
+                <div class="number">2</div>
+                <div class="line"></div>
+                <div class="number">3</div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
